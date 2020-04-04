dbddd05822246b8bede737aa846c6c2d44a9961a
ORTIZFAMILY <ORTIZFAMILY@ORTIZFAMILY-PC>
2020-04-04 00:34:46 -0700

    Updated Keys

---
+++ PianoChordGenerator.Domain/Enums/PianoKeyTypeEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PianoChordGenerator.Domain.Enums
+{
+        public enum PianoKeyTypeEnum
+        {
+            WhiteKey = 1,
+            BlackKey = 2
+        }
+    
+}
--- PianoChordGenerator.Domain/GenerateChordCharts.cs
+++ PianoChordGenerator.Domain/GenerateChordCharts.cs
@@ -3,42 +3,74 @@ using System.IO;
 using System.Linq;
 using System.Xml;
 using System.Xml.Linq;
+using PianoChordGenerator.Domain.Enums;
 //https://www.c-sharpcorner.com/UploadFile/mahesh/create-xml-in-C-Sharp/
+
 namespace PianoChordGenerator.Domain
 {
     public class GenerateChordCharts
     {
+       
         public GenerateChordCharts()
         {
-            using (XmlWriter writer = XmlWriter.Create(@"C:\Temp\pianochord.xml"))
+           
+            string pathname = @"C:\Temp\"+Guid.NewGuid().ToString()+"pianochord.svg";
+
+            using (XmlWriter writer = XmlWriter.Create(pathname))
             {
 
-               writer.WriteStartElement(null, "svg","http://www.w3.org/2000/svg" );
-                writer.WriteEndElement();
-                //writer.WriteElementString("title", "Graphics Programming using GDI+");
-                //writer.WriteElementString("author", "Mahesh Chand");
-                //writer.WriteElementString("publisher", "Addison-Wesley");
+                writer.WriteStartElement(null, "svg", "http://www.w3.org/2000/svg");
+                writer.WriteAttributeString("width", "8.5in");
+                writer.WriteAttributeString("height", "11in");
 
-                //writer.WriteElementString("price", "64.95");
-
-                writer.WriteEndElement();
+                const String y = "36.963078";
+                CreateKey(writer, PianoKeyTypeEnum.WhiteKey, "Chart1WhiteKey1", "7.3248205", y);
+                CreateKey(writer, PianoKeyTypeEnum.WhiteKey, "Chart1WhiteKey2", "13.978922", y);
+                CreateKey(writer, PianoKeyTypeEnum.WhiteKey, "Chart1WhiteKey3", "12.397395", y);
+                CreateKey(writer, PianoKeyTypeEnum.WhiteKey, "Chart1WhiteKey4", "20.633028", y);
+                CreateKey(writer, PianoKeyTypeEnum.WhiteKey, "Chart1WhiteKey5", "20.633028", y);
+                CreateKey(writer, PianoKeyTypeEnum.BlackKey, "Chart1BlackKey1", "19.118507", y);
                 
+
+                writer.WriteEndElement();
+
                 writer.Flush();
             }
 
-            //doc.Save("C:\\Users\\ORTIZFAMILY\\Documents\\ProjectTestDataxmlTest.xml");
+        }
 
-       //     < svg xmlns = "http://www.w3.org/2000/svg" width = "300" height = "200" >
-     
-       //< rect
-       //id = "rect3814"
-       //width = "6.6541033"
-       //height = "36.330299"
-       //x = "7.3248205"
-       //y = "36.963078"
-       //style = "fill:#ffffff;stroke:#000000;stroke-width:0.25942752;stroke-opacity:1" />
+        private void CreateKey(XmlWriter writer, 
+                               PianoKeyTypeEnum keytype,
+                               string id, 
+                               string x, 
+                               string y)
+        {
+            const String KEY_WHITE_WIDTH = "6.6541033";
+            const String KEY_WHITE_HEIGHT = "36.330299";
+            const String KEY_WHITE_STYLE = "fill:#ffffff;stroke:#000000;stroke-width:0.25942752;stroke-opacity:1";
+            const String KEY_BLACK_WIDTH = "3.0012417";
+            const String KEY_BLACK_HEIGHT = "20.712553";
+            const String KEY_BLACK_STYLE = "fill:#000000;fill-opacity:1;stroke:#000000;stroke-width:0.26192313;stroke-opacity:1";
 
+            writer.WriteStartElement("rect");
+            writer.WriteAttributeString("id", id);
 
+            if(keytype==PianoKeyTypeEnum.WhiteKey)
+            {
+                writer.WriteAttributeString("width", KEY_WHITE_WIDTH);
+                writer.WriteAttributeString("height", KEY_WHITE_HEIGHT);                
+                writer.WriteAttributeString("style", KEY_WHITE_STYLE);
+            } else
+            {
+                writer.WriteAttributeString("width", KEY_BLACK_WIDTH);
+                writer.WriteAttributeString("height", KEY_BLACK_HEIGHT);
+                writer.WriteAttributeString("style", KEY_BLACK_STYLE);
+            }
+
+            writer.WriteAttributeString("x", x);
+            writer.WriteAttributeString("y", y);
+
+            writer.WriteEndElement();
         }
         
        
