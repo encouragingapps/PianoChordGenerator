228421b59a37f7fe49250784f0353b6aa2287858
ORTIZFAMILY <ORTIZFAMILY@ORTIZFAMILY-PC>
2020-10-10 16:02:29 -0700

    Vue.Js is wired now.

---
+++ PianoChordGenerator.WebUI/wwwroot/js/pianochordgenerator.js
@@ -0,0 +1,28 @@
+﻿var app = new Vue({
+    el: '#app',
+    mounted: function () {
+        alert('this loaded!');
+    },
+    data: {
+        CardVendor: '',
+        CardNickname: '',
+        CardType: '',
+        CardLimit: '',
+        CardBalance: ''       
+    },
+    methods: {
+        getChords: function (event) {
+            axios.get('/Index/GetChordData')
+                .then((response) => {
+                    console.log(response.data);
+                    console.log(response.status);
+                    console.log(response.statusText);
+                    console.log(response.headers);
+                    console.log(response.config);
+                }).catch(function (error) {
+                    console.log(error);
+                });           
+        }
+
+    }
+})
